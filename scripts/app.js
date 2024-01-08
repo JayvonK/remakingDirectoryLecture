b94aed995b538b7a83d78319f749d050681559c7
JayvonK <jaykhuth@gmail.com>
2024-01-07 22:14:52 -0800

    save

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+// Fetching data from api
+// Then storing the data in array
+// Activate fetch with button click, button click will also create two buttons to iterate through the array(up and down)
+
+//Will need
+//Btn ID
+//2 Div ID's, one for the user info, and one for the created btns
+//User name ID
+//ID for fetch data btn
+//ID for input field
+
+//Keep in mind
+//Use .target, .map, .filter, .append, .createElement, .push
